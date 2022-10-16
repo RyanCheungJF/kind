880b57c3d410d2a6a4b5188434b402fe28fd771a
Ryan Cheung <ryan9cheung@gmail.com>
2022-10-17 00:46:50 +0800

    Add A3 Scripts

---
+++ k8s/a3_setup.sh
@@ -0,0 +1,8 @@
+# assumes a2 setup has been run
+
+echo "Starting Setup for A3..."
+
+echo "1. Distributer YAML:"
+kubectl apply -f k8s/manifests/backend-zone-aware.yaml
+kubectl get nodes -L topology.kubernetes.io/zone
+kubectl get po -lapp=backend-zone-aware -owide --sort-by='.spec.nodeName'
