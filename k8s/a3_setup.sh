c2f3bfb58361b332e47d8d4cc45fccb4435cb498
Ryan Cheung <ryan9cheung@gmail.com>
2022-10-19 15:34:04 +0800

    Add hpa yaml

--- k8s/a3_setup.sh
+++ k8s/a3_setup.sh
@@ -6,3 +6,15 @@ echo "1. Distributer YAML:"
 kubectl apply -f manifests/backend-zone-aware.yaml
 kubectl get nodes -L topology.kubernetes.io/zone
 kubectl get po -lapp=backend-zone-aware -owide --sort-by='.spec.nodeName'
+
+# Metrics server
+# kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+# kubectl get po
+# kubectl describe hpa
+
+# kubectl -nkube-system edit deploy/metrics-server
+# --kubelet-insecure-tls
+# kubectl -nkube-system rollout restart deploy/metrics-server
+
+# kubectl apply -f manifests/backend-hpa.yaml
+# kubectl describe hpa
